93542c0969eae1618764a12de197de7b103fac10
Cristian Davide Conte <64142998+CristianDavideConte@users.noreply.github.com>
2021-09-30 12:03:30 +0200

    deployment test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,8 +13,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "585b51022b87d53ae79530cd979e4d08",
-"/": "585b51022b87d53ae79530cd979e4d08",
+"index.html": "3e49acde7085467385e0c07ceb0989bc",
+"/": "3e49acde7085467385e0c07ceb0989bc",
 "main.dart.js": "a8eb686b1c5e9d0dac8c4034b814b71d",
 "manifest.json": "45b5bda78b9ac7514b9b08669863efcb",
 "version.json": "ec4d3013b3de53624113cd440154e319"
